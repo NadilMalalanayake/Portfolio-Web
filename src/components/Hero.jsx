710f88bc39b33daa6aa8b0e4491bc513f0b7f625
Nadil Malalanayake <nadilviksura@gmail.com>
2024-05-30 20:03:25 +0530

    initial commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,7 +24,7 @@ const Hero = () => {
                 initial="hidden"
                 animate="visible" 
                 className="pb-10 text-6xl font-thin tracking-tight lg:mt-16 lg:text-6xl">
-                    Nadil Malalanayake
+                    Nadil Malalanayaka
                 </motion.h1>
                 <motion.span variants={container(0.5)}
                 initial="hidden"
